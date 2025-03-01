b43bd60b53056b8124d60cd76ab1617b7b2816b0
Sergiy Poddubka <poseal@gmail.com>
2025-03-01 21:19:02 +0200

    add function and preview

--- app/src/main/java/ua/com/poseal/helloworld/MainActivity.kt
+++ app/src/main/java/ua/com/poseal/helloworld/MainActivity.kt
@@ -3,13 +3,30 @@ package ua.com.poseal.helloworld
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello World!")
+            HelloWorld()
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HelloWorld() {
+    Text(
+        text = "Hello World!",
+        fontSize = 25.sp,
+        color = Color.Red,
+        modifier = Modifier.background(Color.Yellow)
+    )
 }
