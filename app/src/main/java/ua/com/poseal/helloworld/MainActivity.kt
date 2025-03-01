a4bd0794099b59f1ed3e66b11d7d4719189c0a69
Sergiy Poddubka <poseal@gmail.com>
2025-03-01 22:03:38 +0200

    custom bias from -1f to 1f

--- app/src/main/java/ua/com/poseal/helloworld/MainActivity.kt
+++ app/src/main/java/ua/com/poseal/helloworld/MainActivity.kt
@@ -9,7 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,7 +32,7 @@ fun HelloWorld() {
             .background(Color.Cyan)
             .fillMaxSize(),
         verticalArrangement = Arrangement.SpaceEvenly,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = BiasAlignment.Horizontal(-0.8f)
     ) {
         Text(
             text = "Hello World!",
