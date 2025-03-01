2884862f0f79b0682e45195a937ccc8fdf1447ff
Sergiy Poddubka <poseal@gmail.com>
2025-03-01 20:35:52 +0200

    delete enableEdgeToEdge(), add  text 'Hello World!'

--- app/src/main/java/ua/com/poseal/helloworld/MainActivity.kt
+++ app/src/main/java/ua/com/poseal/helloworld/MainActivity.kt
@@ -3,12 +3,13 @@ package ua.com.poseal.helloworld
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent { }
+        setContent {
+            Text("Hello World!")
+        }
     }
 }
