edaa41a0eff9ce8f7a76415648d768fc5de31e1b
Sergiy Poddubka <poseal@gmail.com>
2025-03-02 06:06:36 +0200

    padding

--- app/src/main/java/ua/com/poseal/helloworld/MainActivity.kt
+++ app/src/main/java/ua/com/poseal/helloworld/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -14,6 +15,7 @@ import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
@@ -40,6 +42,8 @@ fun HelloWorld() {
             fontSize = 25.sp,
             color = Color.Red,
             modifier = Modifier
+                .background(Color.Blue)
+                .padding(10.dp)
                 .background(Color.Yellow)
                 .align(Alignment.End)
                 .weight(2f)
