ff61b63bd3eeff64a09b35736b90197a3eed9aca
Sergiy Poddubka <poseal@gmail.com>
2025-03-04 15:58:51 +0200

    component TextField, OutlinedTextField without State saving

--- app/src/main/java/ua/com/poseal/helloworld/simple/SimpleActivity.kt
+++ app/src/main/java/ua/com/poseal/helloworld/simple/SimpleActivity.kt
@@ -1,18 +1,20 @@
 package ua.com.poseal.helloworld.simple
 
-import android.content.Context
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 
 class SimpleActivity : ComponentActivity() {
@@ -28,21 +30,29 @@ class SimpleActivity : ComponentActivity() {
 @Preview(showSystemUi = true)
 @Composable
 fun SimpleAppScreen() {
-    val context: Context = LocalContext.current
+    var textValue: String by remember {
+        mutableStateOf("")
+    }
+    var outLinedTextValue: String by remember {
+        mutableStateOf("")
+    }
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center,
     ) {
-        Button(
-            onClick = {
-                Toast.makeText(
-                    context,
-                    "Hello World!",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        ) {
-            Text("Click me!")
+        Column {
+            TextField(
+                value = textValue,
+                onValueChange = { newTextValue ->
+                    textValue = newTextValue
+                }
+            )
+            OutlinedTextField(
+                value = outLinedTextValue,
+                onValueChange = { newTextValue ->
+                    outLinedTextValue = newTextValue
+                }
+            )
         }
     }
 }
