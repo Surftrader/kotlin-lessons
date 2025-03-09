4d48e17a9b697bf6d10c768bde681fde55b3699a
Sergiy Poddubka <poseal@gmail.com>
2025-03-09 16:14:03 +0200

    jetpack compose phases

--- app/src/main/java/ua/com/poseal/helloworld/MainActivity.kt
+++ app/src/main/java/ua/com/poseal/helloworld/MainActivity.kt
@@ -4,16 +4,32 @@ import android.os.Bundle
 import android.os.Parcelable
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlinx.parcelize.Parcelize
-import ua.com.poseal.helloworld.base.Container
 
 @Parcelize
 data class CounterState(
@@ -24,28 +40,57 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AppScreen()
+            var counterState by rememberSaveable {
+                mutableStateOf(CounterState(0))
+            }
+            StatelessCounter(
+                counterValue = counterState.number,
+                onIncrement = { alreadyIncrementedValue ->
+                    counterState = counterState.copy(
+                        number = alreadyIncrementedValue
+                    )
+                }
+            )
         }
     }
 }
 
 @Preview(showSystemUi = true)
 @Composable
-fun AppScreen() {
+fun StatelessCounter(
+    counterValue: Int = 0,
+    onIncrement: (incrementedValue: Int) -> Unit = {},
+) {
+    logCompositionLifecycle(name = "StatelessCounter")
     Column(
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState())
+        modifier = Modifier.fillMaxSize(),
     ) {
-        Container(name = "Buttons example") {
-            ButtonExample()
+        Text(
+            text = counterValue.toString(),
+            fontSize = 60.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Monospace,
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        Button(
+            onClick = { onIncrement(counterValue + 1) }
+        ) {
+            Text(text = "increment", fontSize = 18.sp)
         }
-        Container(name = "TextField example") {
-            TextFieldExample()
-        }
-        Container(name = "CheckBox example") {
-            CheckBoxesExample()
-        }
+        Text(
+            text = "Test",
+            textAlign = TextAlign.Center,
+            color = Color.White,
+            modifier = Modifier
+                .size(width = 60.dp, height = 60.dp)
+                .offset {
+                    println("AAA layout - $counterValue")
+                    IntOffset(y = 20 * (counterValue / 3), x = 0)
+                }
+                //.offset(y = 20.dp * (counterValue / 3))
+                .background(Color.Blue)
+        )
     }
 }
