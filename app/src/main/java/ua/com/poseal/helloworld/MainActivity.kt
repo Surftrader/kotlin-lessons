3a6a683d0fc09e9c342242ee5ac0732d58696272
Sergiy Poddubka <poseal@gmail.com>
2025-03-16 23:00:01 +0200

    Image & Icon & Coin

--- app/src/main/java/ua/com/poseal/helloworld/MainActivity.kt
+++ app/src/main/java/ua/com/poseal/helloworld/MainActivity.kt
@@ -1,101 +1,233 @@
 package ua.com.poseal.helloworld
 
 import android.os.Bundle
-import android.os.Parcelable
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.GenericShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.MailOutline
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-data class CounterState(
-    val number: Int = 0
-) : Parcelable
+import coil.compose.AsyncImage
+import coil.compose.SubcomposeAsyncImage
+import ua.com.poseal.helloworld.base.Container
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var counterState by rememberSaveable {
-                mutableStateOf(CounterState(0))
-            }
-            StatelessCounter(
-                counterValue = counterState.number,
-                onIncrement = { alreadyIncrementedValue ->
-                    counterState = counterState.copy(
-                        number = alreadyIncrementedValue
-                    )
-                }
-            )
+            AppScreen()
         }
     }
 }
 
 @Preview(showSystemUi = true)
 @Composable
-fun StatelessCounter(
-    counterValue: Int = 0,
-    onIncrement: (incrementedValue: Int) -> Unit = {},
-) {
-    logCompositionLifecycle(name = "StatelessCounter")
+fun AppScreen() {
     Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
     ) {
-        Text(
-            text = counterValue.toString(),
-            fontSize = 60.sp,
-            fontWeight = FontWeight.Bold,
-            fontFamily = FontFamily.Monospace,
-        )
         Spacer(modifier = Modifier.height(12.dp))
-        Button(
-            onClick = { onIncrement(counterValue + 1) }
-        ) {
-            Text(text = "increment", fontSize = 18.sp)
-        }
-        val offsetValue by remember {
-            derivedStateOf { counterValue / 3 }
-        }
-        Text(
-            text = "Test",
-            textAlign = TextAlign.Center,
-            color = Color.White,
-            modifier = Modifier
-                .size(width = 60.dp, height = 60.dp)
-                .offset {
-                    println("AAA layout")
-                    IntOffset(y = 20 * offsetValue, x = 0)
-                }
-                //.offset(y = 20.dp * (counterValue / 3))
-                .background(Color.Blue)
+
+        VectorIcon()
+        MaterialIcon()
+        TintedVectorIcon()
+        IconWithModifier()
+        SimpleImage()
+        ContentScaleCropImage()
+        SquareImage()
+        ClippedImage()
+        ClippedWithCustomShapeImage()
+        SimpleAsyncLoadedImage()
+        AsyncLoadedImage()
+
+        Spacer(modifier = Modifier.height(12.dp))
+    }
+}
+
+@Composable
+fun VectorIcon() {
+    Container(name = "Simple vector icon") {
+        Icon(
+            painter = painterResource(R.drawable.health_and_safety_24),
+            contentDescription = "Safety icon"
         )
+    }
+}
+
+@Composable
+fun MaterialIcon() {
+    Container(name = stringResource(R.string.material_icon)) {
+        Icon(
+            imageVector = Icons.Rounded.MailOutline,
+            contentDescription = stringResource(R.string.material_mail_icon),
+            modifier = Modifier.size(48.dp)
+        )
+    }
+}
+
+@Composable
+fun TintedVectorIcon() {
+    Container(name = stringResource(R.string.tinted_vector_icon)) {
+        Icon(
+            painter = painterResource(R.drawable.health_and_safety_24),
+            contentDescription = stringResource(R.string.safety_icon),
+            tint = Color.Red
+        )
+    }
+}
+
+@Composable
+fun IconWithModifier() {
+    Container(name = stringResource(R.string.vector_icon_with_modifier)) {
+        Icon(
+            painter = painterResource(R.drawable.health_and_safety_24),
+            contentDescription = null,
+            modifier = Modifier
+                .size(48.dp)
+                .background(Color.LightGray, CircleShape)
+                .padding(8.dp)
+        )
+    }
+}
+
+@Composable
+fun SimpleImage() {
+    Container(name = stringResource(R.string.simple_image)) {
+        Image(
+            painter = painterResource(R.drawable.nature),
+            contentDescription = stringResource(R.string.random_nature_showcase)
+        )
+    }
+}
+
+@Composable
+fun ContentScaleCropImage() {
+    Container(name = stringResource(R.string.image_with_content_scale)) {
+        Image(
+            painter = painterResource(R.drawable.nature),
+            contentScale = ContentScale.Crop,
+            contentDescription = stringResource(R.string.random_nature_showcase)
+        )
+    }
+}
+
+@Composable
+fun SquareImage() {
+    Container(name = stringResource(R.string.image_cropped_by_aspect_ratio)) {
+        Image(
+            painter = painterResource(R.drawable.nature),
+            contentDescription = stringResource(R.string.random_nature_showcase),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.aspectRatio(1f / 1f)
+        )
+    }
+}
+
+@Composable
+fun ClippedImage() {
+    Container(name = stringResource(R.string.clipped_image)) {
+        Image(
+            painter = painterResource(R.drawable.nature),
+            contentDescription = stringResource(R.string.random_nature_showcase),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .aspectRatio(1f / 1f)
+                .clip(CircleShape)
+        )
+    }
+}
+
+@Composable
+fun ClippedWithCustomShapeImage() {
+    Container(name = stringResource(R.string.clipped_image_with_custom_shape)) {
+        Image(
+            painter = painterResource(R.drawable.nature),
+            contentDescription = stringResource(R.string.random_nature_showcase),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .aspectRatio(1f / 1f)
+                .clip(GenericShape { size, _ ->
+                    moveTo(0f, size.height)
+                    lineTo(size.width / 2, 0f)
+                    lineTo(size.width, size.height)
+                })
+        )
+    }
+}
+
+@Composable
+fun SimpleAsyncLoadedImage() {
+    Container(name = stringResource(R.string.sync_load_by_coil)) {
+        AsyncImage(
+            model = "https://poseal.com.ua/static/img/ava.jpg",
+            contentDescription = null,
+        )
+    }
+}
+
+enum class RequestState {
+    LOAD_NOT_REQUESTED,
+    LOAD_REQUESTED,
+}
+
+@Composable
+fun AsyncLoadedImage() {
+    var state by remember {
+        mutableStateOf(RequestState.LOAD_NOT_REQUESTED)
+    }
+
+    Container(name = stringResource(R.string.sync_load_by_coil)) {
+        when (state) {
+            RequestState.LOAD_NOT_REQUESTED -> LoadButton { state = RequestState.LOAD_REQUESTED }
+            RequestState.LOAD_REQUESTED -> {
+                SubcomposeAsyncImage(
+                    model = "https://poseal.com.ua/static/img/ava.jpg",
+                    contentDescription = null,
+                    loading = {
+                        CircularProgressIndicator()
+                    },
+                    error = {
+                        Text(stringResource(R.string.load_failed), color = Color.Red)
+                    },
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun LoadButton(onClick: () -> Unit) {
+    Button(onClick = onClick) {
+        Text(stringResource(R.string.load))
     }
 }
--- app/src/main/java/ua/com/poseal/helloworld/base/Container.kt
+++ app/src/main/java/ua/com/poseal/helloworld/base/Container.kt
@@ -1,12 +1,15 @@
 package ua.com.poseal.helloworld.base
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -15,32 +18,61 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import ua.com.poseal.helloworld.R
 
 @Composable
 fun Container(
     name: String,
-    content: @Composable ColumnScope.() -> Unit
+    content: @Composable BoxScope.() -> Unit
 ) {
-    Card (
+    Card(
         border = BorderStroke(width = 1.dp, Color.Gray),
         colors = CardDefaults.cardColors(containerColor = Color.Transparent),
         shape = RoundedCornerShape(size = 4.dp),
         modifier = Modifier
-            .fillMaxSize(0.8f)
-            .padding(top = 12.dp)
-    ){
-        Column(modifier = Modifier.padding(8.dp).fillMaxSize()) {
+            .fillMaxWidth()
+            .heightIn(min = 0.dp, max = 100.dp)
+            .padding(vertical = 6.dp, horizontal = 12.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .padding(8.dp)
+                .fillMaxWidth()
+        ) {
+            Box(
+                content = content,
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.weight(0.8f)
+            )
+            Spacer(modifier = Modifier.width(6.dp))
             Text(
                 text = name,
                 fontSize = 14.sp,
                 fontWeight = FontWeight.Bold,
-                modifier = Modifier.align(Alignment.CenterHorizontally)
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .weight(2.0f)
             )
-            Spacer(modifier = Modifier.height(6.dp))
-            content()
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ContainerPreview(){
+    Container(name = "Test") {
+        Image(
+            painter = painterResource(R.drawable.health_and_safety_24),
+            contentDescription = null
+        )
     }
 }
