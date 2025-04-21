3795fdf4981cb9948bc8be52fef89c85dd5ed732
Sergiy Poddubka <poseal@gmail.com>
2025-04-21 23:11:46 +0300

    CompositionLocal Part-1: implicit data transfer

--- app/src/main/java/ua/com/poseal/helloworld/MainActivity.kt
+++ app/src/main/java/ua/com/poseal/helloworld/MainActivity.kt
@@ -4,58 +4,95 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import ua.com.poseal.helloworld.model.UserService
-import ua.com.poseal.helloworld.ui.UserCard
+
+data class AppTheme(
+    val buttonColor: Color,
+    val bgColor: Color
+) {
+
+    companion object {
+        val Light = AppTheme(
+            buttonColor = Color.Blue,
+            bgColor = Color.White
+        )
+        val Dark = AppTheme(
+            buttonColor = Color.Gray,
+            bgColor = Color.Black
+        )
+    }
+}
+
+val LocalAppTheme = compositionLocalOf {
+    AppTheme.Light
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AppScreen()
+            CompositionLocalProvider(
+                LocalAppTheme provides AppTheme.Dark
+            ) {
+                AppScreen()
+            }
         }
     }
 }
 
 @Preview(showSystemUi = true)
 @Composable
-fun AppScreen(userService: UserService = UserService.get()) {
-
-    val userList by userService.getUsers()
-        .collectAsStateWithLifecycle()
+fun AppScreen() {
     val context = LocalContext.current
+    val theme = LocalAppTheme.current
 
-    LazyColumn(
-        contentPadding = PaddingValues(start = 16.dp, end = 16.dp, bottom = 16.dp)
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(theme.bgColor)
     ) {
-        items(
-            items = userList,
-            key = { user -> user.id },
-        ) { user ->
-            UserCard(
-                user = user,
-                onUserClicked = {
-                    Toast.makeText(
-                        context,
-                        context.getString(R.string.clicked_on) + ' ' + user.name,
-                        Toast.LENGTH_SHORT
-                    ).show()
-                },
-                onUserDeleted = {
-                    userService.removeUser(user)
-                },
-                modifier = Modifier.padding(top = 16.dp)
-            )
-        }
+        CustomButton(
+            onClick = {
+                Toast.makeText(
+                    context,
+                    "Hello World",
+                    Toast.LENGTH_SHORT
+                ).show()
+            },
+            text = "Hello"
+        )
+    }
+}
+
+@Composable
+fun CustomButton(
+    onClick: () -> Unit,
+    text: String,
+    modifier: Modifier = Modifier,
+) {
+    val theme = LocalAppTheme.current
+
+    Button(
+        onClick = onClick,
+        modifier = Modifier,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = theme.buttonColor
+        )
+    ) {
+        Text(text = text)
     }
 }
