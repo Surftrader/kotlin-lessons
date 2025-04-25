2465fb673028c63517a4e9939640efd7bfbc0f2b
Surftrader <poseal@gmail.com>
2025-04-25 16:48:45 +0300

    add material design styles

--- app/src/main/java/ua/com/poseal/helloworld/MainActivity.kt
+++ app/src/main/java/ua/com/poseal/helloworld/MainActivity.kt
@@ -3,9 +3,15 @@ package ua.com.poseal.helloworld
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import ua.com.poseal.helloworld.ui.theme.AppTheme
 import ua.com.poseal.helloworld.util.PreviewWithInsets
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +26,17 @@ class MainActivity : ComponentActivity() {
 @Preview(showSystemUi = true)
 @Composable
 fun AppScreen() {
-    PreviewWithInsets {
-        Text(text = "Hello World!")
+    AppTheme {
+        PreviewWithInsets {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center) {
+                Button(
+                    onClick = { }
+                ) {
+                    Text(text = "Hello World!")
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/ua/com/poseal/helloworld/ui/theme/Type.kt
@@ -0,0 +1,5 @@
+package ua.com.poseal.helloworld.ui.theme
+
+import androidx.compose.material3.Typography
+
+val AppTypography = Typography()
