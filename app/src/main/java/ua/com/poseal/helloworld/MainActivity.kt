27ca879b9c9ea47cdfd337a3c48f4d3fc1908388
Surftrader <poseal@gmail.com>
2025-04-28 00:44:54 +0300

    Scaffold - example

--- app/src/main/java/ua/com/poseal/helloworld/MainActivity.kt
+++ app/src/main/java/ua/com/poseal/helloworld/MainActivity.kt
@@ -3,46 +3,153 @@ package ua.com.poseal.helloworld
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
-import ua.com.poseal.helloworld.util.PreviewWithInsets
+import ua.com.poseal.helloworld.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AppScreen()
+            AppTheme {
+                AppScreen()
+            }
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppScreen() {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(R.string.scaffold_app),
+                        fontWeight = FontWeight.Bold,
+                    )
+                },
+                colors = TopAppBarDefaults.largeTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                navigationIcon = {
+                    IconButton(
+                        onClick = { /* TODO */ }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Menu,
+                            contentDescription = stringResource(R.string.main_menu),
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(
+                        onClick = { /* TODO */ }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.MoreVert,
+                            contentDescription = stringResource(R.string.more_actions),
+                        )
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /* TODO */ }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(R.string.add_item),
+                )
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End,
+        bottomBar = {
+            NavigationBar {
+                NavigationBarItem(
+                    selected = true,
+                    label = { Text(stringResource(R.string.items)) },
+                    onClick = { /* TODO */ },
+                    icon = {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.List,
+                            contentDescription = stringResource(R.string.items)
+                        )
+                    }
+                )
+                NavigationBarItem(
+                    selected = false,
+                    label = { Text(stringResource(R.string.settings)) },
+                    onClick = { /* TODO */ },
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Default.Settings,
+                            contentDescription = stringResource(R.string.settings)
+                        )
+                    }
+                )
+                NavigationBarItem(
+                    selected = false,
+                    label = { Text(stringResource(R.string.profile)) },
+                    onClick = { /* TODO */ },
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Default.AccountBox,
+                            contentDescription = stringResource(R.string.profile)
+                        )
+                    }
+                )
+            }
+        },
+        snackbarHost = { }
+    ) { paddingValues ->
+        // main content:
+        Text(
+            text = "Hello World",
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp,
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+                .wrapContentHeight(),
+        )
     }
 }
 
 @Preview(showSystemUi = true)
 @Composable
-fun AppScreen() {
-    PreviewWithInsets() {
-        Scaffold(
-            topBar = { Text(text = "Top bar", fontSize = 30.sp) },
-            bottomBar = { Text(text = "Bottom bar", fontSize = 30.sp) },
-            floatingActionButton = { Text(text = "Floating button", fontSize = 30.sp) },
-            floatingActionButtonPosition = FabPosition.Center,
-            snackbarHost = { }
-        ) { paddingValues ->
-            // main content:
-            Box(
-                modifier = Modifier
-                    .padding(paddingValues)
-                    .fillMaxSize()
-            ) {
-                // play here
-            }
-        }
+fun AppScreenPreview() {
+    AppTheme(darkTheme = true) {
+        AppScreen()
     }
 }
