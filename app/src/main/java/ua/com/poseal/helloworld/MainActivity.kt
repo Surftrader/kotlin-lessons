7744cf2fa58312f18130662c696c178f8c74cc66
Surftrader <poseal@gmail.com>
2025-04-28 20:22:25 +0300

    TopAppBar (scrollBehavior)

--- app/src/main/java/ua/com/poseal/helloworld/MainActivity.kt
+++ app/src/main/java/ua/com/poseal/helloworld/MainActivity.kt
@@ -3,44 +3,36 @@ package ua.com.poseal.helloworld
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FabPosition
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import ua.com.poseal.helloworld.ui.theme.AppTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            AppTheme {
-                AppScreen()
-            }
+            AppScreen()
         }
     }
 }
@@ -48,108 +40,71 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppScreen() {
-    Scaffold(
-        topBar = {
-            CenterAlignedTopAppBar(
-                title = {
-                    Text(
-                        text = stringResource(R.string.scaffold_app),
-                        fontWeight = FontWeight.Bold,
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection)
+    ) {
+        TopAppBar(
+            title = {
+                Text(
+                    text = "Screen Title",
+                    fontWeight = FontWeight.Bold,
+                )
+            },
+            navigationIcon = {
+                IconButton(
+                    onClick = { /*TODO*/ }
+                ) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Go Back",
                     )
-                },
-                colors = TopAppBarDefaults.largeTopAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                ),
-                navigationIcon = {
-                    IconButton(
-                        onClick = { /* TODO */ }
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Menu,
-                            contentDescription = stringResource(R.string.main_menu),
-                        )
-                    }
-                },
-                actions = {
-                    IconButton(
-                        onClick = { /* TODO */ }
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.MoreVert,
-                            contentDescription = stringResource(R.string.more_actions),
-                        )
-                    }
                 }
-            )
-        },
-        floatingActionButton = {
-            FloatingActionButton(
-                onClick = { /* TODO */ }
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Add,
-                    contentDescription = stringResource(R.string.add_item),
-                )
-            }
-        },
-        floatingActionButtonPosition = FabPosition.End,
-        bottomBar = {
-            NavigationBar {
-                NavigationBarItem(
-                    selected = true,
-                    label = { Text(stringResource(R.string.items)) },
-                    onClick = { /* TODO */ },
-                    icon = {
-                        Icon(
-                            imageVector = Icons.AutoMirrored.Filled.List,
-                            contentDescription = stringResource(R.string.items)
-                        )
-                    }
-                )
-                NavigationBarItem(
-                    selected = false,
-                    label = { Text(stringResource(R.string.settings)) },
-                    onClick = { /* TODO */ },
-                    icon = {
-                        Icon(
-                            imageVector = Icons.Default.Settings,
-                            contentDescription = stringResource(R.string.settings)
-                        )
-                    }
-                )
-                NavigationBarItem(
-                    selected = false,
-                    label = { Text(stringResource(R.string.profile)) },
-                    onClick = { /* TODO */ },
-                    icon = {
-                        Icon(
-                            imageVector = Icons.Default.AccountBox,
-                            contentDescription = stringResource(R.string.profile)
-                        )
-                    }
-                )
-            }
-        },
-        snackbarHost = { }
-    ) { paddingValues ->
-        // main content:
-        Text(
-            text = "Hello World",
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp,
-            modifier = Modifier
-                .padding(paddingValues)
-                .fillMaxSize()
-                .wrapContentHeight(),
+            },
+            actions = {
+                IconButton(
+                    onClick = { /*TODO*/ }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Settings,
+                        contentDescription = null,
+                    )
+                }
+                IconButton(
+                    onClick = { /*TODO*/ }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.AccountBox,
+                        contentDescription = null,
+                    )
+                }
+            },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                navigationIconContentColor = Color.Red,
+                actionIconContentColor = Color.Blue,
+                scrolledContainerColor = Color.Yellow,
+            ),
+            scrollBehavior = scrollBehavior,
         )
+
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(100) { item ->
+                Text(
+                    text = "Item #${item + 1}",
+                    modifier = Modifier.padding(12.dp),
+                )
+            }
+        }
     }
 }
 
 @Preview(showSystemUi = true)
 @Composable
 fun AppScreenPreview() {
-    AppTheme(darkTheme = true) {
-        AppScreen()
-    }
+    AppScreen()
 }
