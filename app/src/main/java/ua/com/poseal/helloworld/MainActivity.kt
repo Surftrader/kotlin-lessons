af819e3a82d7765cb1d42e72523e9368ec3d3655
Surftrader <poseal@gmail.com>
2025-04-28 21:03:08 +0300

    NavigationBar parameters

--- app/src/main/java/ua/com/poseal/helloworld/MainActivity.kt
+++ app/src/main/java/ua/com/poseal/helloworld/MainActivity.kt
@@ -3,34 +3,32 @@ package ua.com.poseal.helloworld
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             AppScreen()
         }
@@ -40,66 +38,106 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppScreen() {
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .nestedScroll(scrollBehavior.nestedScrollConnection)
+        modifier = Modifier.fillMaxSize()
     ) {
-        LargeTopAppBar(
-            title = {
-                Text(
-                    text = "Screen Title",
-                    fontWeight = FontWeight.Bold,
-                )
-            },
-            navigationIcon = {
-                IconButton(
-                    onClick = { /*TODO*/ }
-                ) {
+        TopAppBar(
+            title = { Text(text = "Random Title") },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer
+            )
+        )
+
+        Surface(
+            color = MaterialTheme.colorScheme.background,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1.0f)
+        ) {
+            // TODO: content here
+        }
+
+        NavigationBar {
+
+            NavigationBarItem(
+                selected = true,
+                onClick = { /*TODO*/ },
+                icon = {
                     Icon(
-                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                        contentDescription = "Go Back",
+                        imageVector = Icons.AutoMirrored.Filled.List,
+                        contentDescription = null,
                     )
-                }
-            },
-            actions = {
-                IconButton(
-                    onClick = { /*TODO*/ }
-                ) {
+                },
+                label = {
+                    Text(text = stringResource(R.string.items))
+                },
+                enabled = true,
+                alwaysShowLabel = true, // when selected = false - label(text) will be invisible
+                colors = NavigationBarItemDefaults.colors(
+                    unselectedIconColor = Color.Blue, // when selected = false
+                    selectedIconColor = Color.Red, // when selected = true
+                    disabledIconColor = Color.LightGray, // when enabled = false
+                    // The same for label:
+                    unselectedTextColor = Color.Blue,
+                    selectedTextColor = Color.Red,
+                    disabledTextColor = Color.LightGray,
+
+                    indicatorColor = Color.Yellow // when selected = true
+                )
+            )
+
+            NavigationBarItem(
+                selected = false,
+                onClick = { /*TODO*/ },
+                icon = {
                     Icon(
                         imageVector = Icons.Default.Settings,
                         contentDescription = null,
                     )
-                }
-                IconButton(
-                    onClick = { /*TODO*/ }
-                ) {
+                },
+                label = {
+                    Text(text = stringResource(R.string.settings))
+                },
+                enabled = true,
+                alwaysShowLabel = false,
+                colors = NavigationBarItemDefaults.colors(
+                    unselectedIconColor = Color.Blue,
+                    selectedIconColor = Color.Red,
+                    disabledIconColor = Color.LightGray,
+                    unselectedTextColor = Color.Blue,
+                    selectedTextColor = Color.Red,
+                    disabledTextColor = Color.LightGray,
+                    indicatorColor = Color.Yellow
+                )
+            )
+
+            NavigationBarItem(
+                selected = false,
+                onClick = { /*TODO*/ },
+                icon = {
                     Icon(
                         imageVector = Icons.Default.AccountBox,
                         contentDescription = null,
                     )
-                }
-            },
-            colors = TopAppBarDefaults.topAppBarColors(
-                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
-                navigationIconContentColor = Color.Red,
-                actionIconContentColor = Color.Blue,
-                scrolledContainerColor = Color.Yellow,
-            ),
-            scrollBehavior = scrollBehavior,
-        )
-
-        LazyColumn(modifier = Modifier.fillMaxSize()) {
-            items(100) { item ->
-                Text(
-                    text = "Item #${item + 1}",
-                    modifier = Modifier.padding(12.dp),
+                },
+                label = {
+                    Text(text = stringResource(R.string.profile))
+                },
+                enabled = false,
+                alwaysShowLabel = true,
+                colors = NavigationBarItemDefaults.colors(
+                    unselectedIconColor = Color.Blue,
+                    selectedIconColor = Color.Red,
+                    disabledIconColor = Color.LightGray,
+                    unselectedTextColor = Color.Blue,
+                    selectedTextColor = Color.Red,
+                    disabledTextColor = Color.LightGray,
+                    indicatorColor = Color.Yellow
                 )
-            }
+            )
         }
+
     }
 }
 
