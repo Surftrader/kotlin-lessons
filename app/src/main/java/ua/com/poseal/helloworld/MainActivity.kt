290a6708463f5bee5f87dff5d0655e903f83ccf3
Surftrader <poseal@gmail.com>
2025-04-29 18:43:07 +0300

    DropdownMenu, DropdownMenuItem - additional params

--- app/src/main/java/ua/com/poseal/helloworld/MainActivity.kt
+++ app/src/main/java/ua/com/poseal/helloworld/MainActivity.kt
@@ -7,13 +7,19 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MenuDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -26,7 +32,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.PopupProperties
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -65,7 +73,9 @@ fun AppScreen() {
 
             DropdownMenu(
                 expanded = showPopupMenu,
-                onDismissRequest = { showPopupMenu = false }
+                onDismissRequest = { showPopupMenu = false },
+                offset = DpOffset(x = 0.dp, y  =0.dp),
+                properties = PopupProperties()
             ) {
                 DropdownMenuItem(
                     text = { Text("Menu Item 111") },
@@ -77,6 +87,33 @@ fun AppScreen() {
                         ).show()
                         showPopupMenu = false
                     },
+                    enabled = false,
+                    // Add icon at the start
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Delete,
+                            contentDescription = null,
+                        )
+                    },
+                    // Add icon at the end
+                    trailingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Done,
+                            contentDescription = null,
+                        )
+                    },
+                    contentPadding = PaddingValues(40.dp),
+                    colors = MenuDefaults.itemColors(
+                        textColor = Color.Black,
+                        leadingIconColor = Color.Black,
+                        trailingIconColor = Color.Black,
+                        disabledTextColor = Color.LightGray,
+                        disabledLeadingIconColor = Color.LightGray,
+                        disabledTrailingIconColor = Color.LightGray,
+
+                    ),
+                    // Listener for menu actions
+                    // interactionSource = ,
                 )
                 HorizontalDivider(modifier = Modifier, thickness = 2.dp, color = Color.LightGray)
                 DropdownMenuItem(
