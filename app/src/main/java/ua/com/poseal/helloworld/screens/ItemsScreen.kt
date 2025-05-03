43ed24bbc58e463d89c5a60f6548b4489db6c0ea
Surftrader <poseal@gmail.com>
2025-05-03 21:18:34 +0300

    Navigation optimization

---
+++ app/src/main/java/ua/com/poseal/helloworld/screens/ItemsScreen.kt
@@ -0,0 +1,63 @@
+package ua.com.poseal.helloworld.screens
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import ua.com.poseal.helloworld.ItemsRepository
+import ua.com.poseal.helloworld.R
+
+@Composable
+fun ItemsScreen() {
+    val itemsRepository = ItemsRepository.get()
+    val items by itemsRepository.getItems().collectAsStateWithLifecycle()
+    val isEmpty by remember {
+        derivedStateOf { items.isEmpty() }
+    }
+    ItemsContent(isItemsEmpty = isEmpty, items = { items })
+}
+
+@Composable
+fun ItemsContent(
+    isItemsEmpty: Boolean,
+    items: () -> List<String>,
+) {
+    if (isItemsEmpty) {
+        Text(
+            text = stringResource(R.string.no_items),
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentHeight()
+        )
+    } else {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            items(items.invoke()) { item ->
+                Text(
+                    text = item,
+                    modifier = Modifier.padding(all = 8.dp),
+                )
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun ItemsScreenPreview() {
+    ItemsScreen()
+}
---
+++ app/src/main/java/ua/com/poseal/helloworld/screens/SettingsScreen.kt
@@ -0,0 +1,28 @@
+package ua.com.poseal.helloworld.screens
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SettingsScreen() {
+    Text(
+        text = "Settings Screen",
+        textAlign = TextAlign.Center,
+        fontSize = 20.sp,
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentHeight()
+    )
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun SettingsScreenPreview() {
+    SettingsScreen()
+}
